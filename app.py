26c27160fdf4aa30653d80ad80da28fcf3e33af3
David Barnett <david@barnett.net.nz>
2016-07-31 09:26:16 +1200

    update referal to use parameters

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python2
 
-from flask import Flask, render_template, redirect, url_for
-app = Flask('Bubble Kiwi')
+from flask import Flask, request, render_template, redirect, url_for
+app = Flask("Bubble Kiwi")
 
 @app.errorhandler(404)
 def not_found(error):
-    return redirect('http://bubble.kiwi/')
+    return redirect("http://bubble.kiwi/")
 
-@app.route('/<refer>')
+@app.route("/")
+@app.route("/<refer>")
 def referal(refer=None):
-    return render_template("redirect.html")
+    title = request.args.get("t", "Bubble")
+    image_url = request.args.get("i", "Bursting the Bubble")
+    description = request.args.get("d", "")
+
+    return render_template("redirect.html", title=title, image=image_url, description=description)
 
 if __name__ == "__main__":
     app.run()
